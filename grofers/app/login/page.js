0bfc09cc25f333ae07bf6e6848894cb0f97554f9
Sebastian Gutierrez <rodrigo.gutierrez.v@tecsup.edu.pe>
2025-11-18 00:38:13 -0500

    fix: disable prerendering for login and auth-success

--- grofers/app/login/page.js
+++ grofers/app/login/page.js
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect, useMemo } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { toast } from "sonner";
